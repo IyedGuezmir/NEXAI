43c76c7fe22b3756ced9324591ffe8de4df6c9c3
Iyeeed <iyed.guezmir@esprit.tn>
2025-04-29 14:52:44 +0100

    Initial commit

---
+++ Architecture/project-structure-script.sh
@@ -0,0 +1,22 @@
+# Create project directory
+
+mkdir nexai\src\agents
+mkdir nexai\src\data
+mkdir PI\nexai\src\models
+mkdir PI\nexai\src\utils
+mkdir PI\nexai\notebooks
+mkdir PI\nexai\static
+mkdir PI\nexai\templates
+mkdir PI\nexai\test
+
+# Create placeholder files
+
+echo. > nexai\README.md
+echo. > PI\requirements.txt
+echo. > PI\nexai\src\agents\Assistant_Agent.py
+echo. > PI\nexai\src\agents\Search_Agent.py
+echo. > PI\nexai\notebooks\performance_stimulator.ipynb
+echo. > PI\nexai\templates\index.html
+echo. > PI\nexai\static\style.css
+echo. > PI\app.py
+
